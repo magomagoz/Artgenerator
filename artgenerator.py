96e887c141d6a90fcaf18d1cc0a3aca6987012cc
magomagoz <magostinienrico@gmail.com>
2026-03-12 16:20:19 +0100

    Update artgenerator.py

--- artgenerator.py
+++ artgenerator.py
@@ -145,8 +145,6 @@ if st.button("Genera Visione Artistica"):
                     mime="application/pdf"
                 )
         
-            except Exception as e:
-                st.error(f"Errore durante la generazione dell'immagine: {e}")
 
 if st.session_state.res:
     r = st.session_state.res
